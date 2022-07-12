ce94236c054d34be2f26d8ddad8c6d4999b3e4e2
Anton <antondanik32@mail.ru>
2022-07-12 14:22:24 +0300

    create WatcherService

---
+++ src/main/java/com/bsuir/crypto_currency_watcher/service/WatcherService.java
@@ -0,0 +1,9 @@
+package com.bsuir.crypto_currency_watcher.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WatcherService {
+}
