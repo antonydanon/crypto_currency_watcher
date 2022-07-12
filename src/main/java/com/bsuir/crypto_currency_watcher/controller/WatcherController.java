13c6e576b62bd2ad5faa3ff9c978f34adc009c71
Anton <antondanik32@mail.ru>
2022-07-12 14:21:05 +0300

    create WatcherController

---
+++ src/main/java/com/bsuir/crypto_currency_watcher/controller/WatcherController.java
@@ -0,0 +1,11 @@
+package com.bsuir.crypto_currency_watcher.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/watchers")
+@RequiredArgsConstructor
+public class WatcherController {
+}
